d9eb233c341ebc2f040906d1c259b655cca1fea9
Jorge Romero <jorgeromero98@outlook.com>
2024-01-11 19:33:10 -0600

    ya se muestran los gatitos favoritos

--- main.js
+++ main.js
@@ -6,6 +6,14 @@ const catImage = document.querySelector('.catImage');
 let user = localStorage.getItem('user');
 let currentCat;
 
+function generateUID(){
+  const randomNum = Math.random().toString(36).substring(2);
+  const timestamp = (new Date()).getTime().toString(36);
+  const uid = randomNum + timestamp;
+
+  return uid;
+}
+
 async function getCat(){
 	const response = await fetch(URL+'images/search');
 	const cat = await response.json();
@@ -13,19 +21,19 @@ async function getCat(){
 }
 
 async function getFavs(){
-	const response = await fetch(URL+`favourites?limit=9&sub_id=user-123&order=DESC`,{
+	const response = await fetch(URL+`favourites?limit=9&sub_id=${user}&order=DESC`,{
     headers:{
       "content-type":"application/json",
       'x-api-key': APIkey,
     }
   });
-	const favs = await response.json();
-	console.log(favs);
+	
+  const favs = await response.json();
+
+  return favs;
 }
 
 async function setFav(){
-  console.log(currentCat);
-
   const rawBody = JSON.stringify({ 
     "image_id":currentCat,
     "sub_id":user
@@ -40,28 +48,59 @@ async function setFav(){
       },
       body: rawBody,
     }
-  )
-  console.log('nuevo gato favorito',newFavourite);  
+  ) 
   showCat();
+  showFavoritesCats();
 }
 
 async function showCat(){
 	const cat = await getCat();
   currentCat = cat[0].id;
-  console.log(cat[0]);
+
 	catImage.style.backgroundImage = `url('${cat[0].url}')`; 
 }
 
-function generateUID(){
-  const randomNum = Math.random().toString(36).substring(2);
-  const timestamp = (new Date()).getTime().toString(36);
-  const uid = randomNum + timestamp;
+function clearSectionFavs(sectionFavs,gallery) {
+  // Eliminar el párrafo existente si hay favoritos
+  const existingParagraph = sectionFavs.querySelector('p');
+  if (existingParagraph) {
+    sectionFavs.removeChild(existingParagraph);
+  }
 
-  return uid;
+  // Eliminar imágenes existentes
+  const existingImages = gallery.querySelectorAll('img');
+  existingImages.forEach(img => {
+    gallery.removeChild(img);
+  });
+}
+
+async function showFavoritesCats(){
+  const cats = await getFavs();
+  
+  console.log(cats);
+  const gallery = document.querySelector('#gallery');
+  const sectionFavs = document.querySelector('#favs');
+
+  clearSectionFavs(sectionFavs,gallery);
+
+  if(cats.length>0){
+    cats.forEach(cat => {
+      const img = document.createElement('img');
+      img.src = cat.image.url;
+      img.classList.add('item');
+      
+      gallery.appendChild(img);
+    });
+  } else {
+    const p = document.createElement('p');
+    p.textContent = "You don't have favorites yet...";
+    sectionFavs.appendChild(p);
+  }
 }
 
 function main() {
 	showCat();
+  showFavoritesCats();
 
   if(!user){
     localStorage.setItem('user',generateUID());
@@ -81,6 +120,5 @@ function handleClickNewFavoriteCat() {
 
 btnRefreshCat.addEventListener('click',handleClickShowCat);
 btnNewFavoriteCat.addEventListener('click',handleClickNewFavoriteCat);
-
 
 main();
